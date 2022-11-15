3162b53176570fcfc850f3633b7eaba0d0741dc0
Phil Jonak <pjonak1@gmail.com>
2022-11-15 09:13:47 -0600

    Remove unnecessary not null import

--- blizz-api/blizz-service/blizz-service-beer-recipe/src/main/java/com/blizz/service/beer/recipe/dto/PostBeerRecipeRequest.java
+++ blizz-api/blizz-service/blizz-service-beer-recipe/src/main/java/com/blizz/service/beer/recipe/dto/PostBeerRecipeRequest.java
@@ -1,7 +1,6 @@
 package com.blizz.service.beer.recipe.dto;
 
 import com.blizz.persistence.beer.model.*;
-import org.springframework.lang.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
